94fa051349690da12e21b90527e5e5fb984d85d2
Thomas Santos <thomas@socialeffort.co>
2017-10-19 04:30:40 -0400

    use MongoClient to establish connection to movies. set server to port 3000

--- alltogether/app.js
+++ alltogether/app.js
@@ -8,3 +8,24 @@ app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
+
+  assert.equal(null, err);
+  console.log("Successfully connected to MongoDB.");
+
+  app.get('/', function(req, res){
+
+    db.collection('movies').find({}).toArray(function(err, docs) {
+      res.render('movies', { 'movies': docs } );
+    });
+  });
+
+  app.use(function(req, res){
+    res.sendStatus(404);
+  });
+
+  var server = app.listen(3000, function() {
+    var port = server.address().port;
+    console.log('Express server listening on port %s.', port);
+  });
+});
