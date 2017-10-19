e23e7f944237d30fa21bdc0cfcc293580caf05cb
Thomas Santos <thomas@socialeffort.co>
2017-10-19 01:00:51 -0400

    refactor folder structure. add introtonpm folder

--- webservsimple/app.js
+++ webservsimple/app.js
@@ -1,1 +1,11 @@
+// init server with builtin 
 var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("Hello, world!");
+});
+
+server.listen(8000);
+
+console.log("Server running on PORT 8000");
