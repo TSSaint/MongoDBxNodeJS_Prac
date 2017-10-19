11f62ae12e661fbc02b16cc475891f3acac6e2fb
Thomas Santos <thomas@socialeffort.co>
2017-10-19 01:35:07 -0400

    add nodejsdriver folder. add and set app.js. Require mongo and assert packages

---
+++ nodejsdriver/app.js
@@ -0,0 +1,25 @@
+var MongoClient = require("mongodb").MongoClient
+var assert = require("assert");
+
+var url = "mongodb://localhost:27017/video";
+
+MongoClient.connect(url, function(err, db) {
+  
+  assert.equal(null, err);
+  console.log("Successfully connected to server on ${url}!");
+  
+  // find documents in collection db
+  db.collection("movies").find({}).toArray(function(err, docs) {
+    
+    // print the docs
+    docs.forEact(function(doc) {
+      console.log(doc.title);
+    });
+    
+    // close db
+    db.close();
+  });
+
+  // So far so good (y)
+  console.log("Successfully called called find()");
+});
