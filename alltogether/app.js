0f829ec5edb6f8513c17626b9acdc29976acf17f
Thomas Santos <thomas@socialeffort.co>
2017-10-19 04:26:34 -0400

    set view engine to nunjucks

--- alltogether/app.js
+++ alltogether/app.js
@@ -3,3 +3,8 @@ var app = express();
 var engines = require('consolidate');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
