262a0a80fc70d860e13794c62ac21f12b0e56a4f
Thomas Santos <thomas@socialeffort.co>
2017-10-19 15:53:50 -0400

    change var

--- gethandling_express/app.js
+++ gethandling_express/app.js
@@ -18,7 +18,7 @@ app.get('/:name', function(req, res, next) {
   var name = req.params.name;
   var getvar1 = req.query.getvar1;
   var getvar2 = req.query.getvar2;
-  res.render('hello', { name : name, getvar : getvar1, getvar2 : getvar2 });
+  res.render('hello', { name : name, getvar1 : getvar1, getvar2 : getvar2 });
 });
 
 app.use(errorHandler);
