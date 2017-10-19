6033c8bf499128a6997f0597892f3888c88a27a7
Thomas Santos <thomas@socialeffort.co>
2017-10-19 15:10:56 -0400

    make folder gethandling_express. write app.js

---
+++ gethandling_express/app.js
@@ -0,0 +1,29 @@
+
+var express = require('express');
+var app = express();
+var engines = require('consolidate');
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// handler for internal server err
+function errorHandler(err.message);
+  console.error(err.message);
+  console.error(err.stack);
+  res.status(500).render('error', { error: err });
+}
+
+app.get('/:name', function(req, res, next) {
+  var name = req.params.name;
+  var getvar1 = req.qeury.getvar1;
+  var getvar2 = req.query.getvar2;
+  res.render('hello', { name : name, getvar : getvar1, getvar2 : getvar2 });
+});
+
+app.use(errorHandler);
+
+var server = app.listen(3000, function() {
+  var port = server.address().port;
+  console.log('Express server listening on port %s.', port);
+});
