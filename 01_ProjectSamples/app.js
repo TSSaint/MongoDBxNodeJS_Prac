1ee164fc00c6fe30dcbb2be8fee9fbada96baa7d
Thomas Santos <thomas@socialeffort.co>
2017-10-29 23:56:09 -0400

    add Mongo

--- 01_ProjectSamples/app.js
+++ 01_ProjectSamples/app.js
@@ -5,3 +5,25 @@ var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// handle internal errors
+function errorHandler(err, req, res, next) {
+    console.error(err.message);
+    console.error(err.stack);
+    res.status(500).render('error_template', { error: err });
+}
+
+MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
+    assert.equal(null, err);
+    console.log("Successful connection established with MongoDB.");
+
+    app.post('/add_movie', function(req, res, next) {
+        var title = req.body.title;
+        var year = req.body.year;
+        var imdb = req.body.imdb;
+    });
+});
