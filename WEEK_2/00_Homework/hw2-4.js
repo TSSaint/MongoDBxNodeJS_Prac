0d6eca88c3a7fc735863883b621be0d58fda924d
Thomas Santos <thomas@socialeffort.co>
2017-10-31 05:56:57 -0400

    add solution to hw2-4.js

--- WEEK_2/00_Homework/hw2-4.js
+++ WEEK_2/00_Homework/hw2-4.js
@@ -1,7 +1,6 @@
 // add solution hw2-4.js
-// How many documents in the video.movieDetails collection lists just the following two genres: "Comedy" and "Crime", where "Comedy" is listed first?
-// Find out how many documents have two genres
-db.movieDetails.find({ genres: {$size: 2}}).pretty();
-// Check out of these if any list Comedy and Crime as genres
 
+// How many documents in the video.movieDetails collection lists just the following two genres: "Comedy" and "Crime", where "Comedy" is listed first?
+db.movieDetails.find({ "genres": ["Comedy", "Crime"]}).count();
+// 20
 
