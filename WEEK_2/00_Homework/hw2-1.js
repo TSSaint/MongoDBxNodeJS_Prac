6b522dfa63e56bda9fd28bac657d6b18827dc36d
Thomas Santos <thomas@socialeffort.co>
2017-10-31 04:27:17 -0400

    add more detail

--- WEEK_2/00_Homework/hw2-1.js
+++ WEEK_2/00_Homework/hw2-1.js
@@ -1,4 +1,5 @@
 // hw2-1 solution
+
 // Which of the choices below is the title of a movie from the year 2013 that is rated PG-13 and won no awards?
 // In mongo, run:
-db.movieDetails.find({ $and: [ { "awards.wins": {$eq: 0 } }, { "rated": {$eq: "PG-13"} }, { "year": {$eq: 2013 } } ] }, { title: 1, _id: 0 }).pretty();
+db.movieDetails.find({ $and: [ {"awards.wins": {$eq: 0} }, {"rated":{$eq: "PG-13"}}, {"year": {$eq: 2013} } ] }, { title: 1, rated:1, year:1, awards:1,_id: 0 }).pretty();:
