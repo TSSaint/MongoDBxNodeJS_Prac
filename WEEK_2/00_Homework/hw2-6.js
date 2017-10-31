6b1381483bbff6d4fc2c2ece583b7ef4c7254096
Thomas Santos <thomas@socialeffort.co>
2017-10-31 06:32:13 -0400

    add hw2-6.js solution

---
+++ WEEK_2/00_Homework/hw2-6.js
@@ -0,0 +1,3 @@
+// solution for hw2-6.js
+// Suppose you wish to update the value of the "plot" field for one document in the collection to correct a typo. Which update operator would you use to do this?
+Answer: $set
