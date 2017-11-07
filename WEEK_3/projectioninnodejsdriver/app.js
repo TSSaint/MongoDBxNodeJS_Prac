086d82063676d13c7ff85e1dc7b2f5245983ccb1
Thomas Santos <thomas@socialeffort.co>
2017-11-07 00:37:50 -0500

    update app.js. install mongodb

--- WEEK_3/projectioninnodejsdriver/app.js
+++ WEEK_3/projectioninnodejsdriver/app.js
@@ -1,22 +1,26 @@
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
 
-MongoClient.connec('mongodb://localhost:27017/crunchbase'. function(err, db) {
+MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
 
     assert.equal(err, null);
-    console.log("Aye aye, captain - connected to MongoDB!");
+    console.log("Successfully connected to MongoDB.");
 
+    // holds query for crunchbase db
     var query = {"category_code": "biotech"};
+    // holds specific projection for db.crunchbase
     var projection = {"name": 1, "category_code": 1, "_id": 0};
-
+    // tells cursor what to do
     var cursor = db.collection('companies').find(query);
     cursor.project(projection);
 
+    // print each item recieved in query
     cursor.forEach(
         function(doc) {
             console.log(doc.name + " is a " + doc.category_code + " company.");
             console.log(doc);
         },
+        // assertion test and function if fails
         function(err) {
             assert.equal(err, null);
             return db.close();
