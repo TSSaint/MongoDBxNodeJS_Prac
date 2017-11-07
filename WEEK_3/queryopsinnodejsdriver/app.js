795204637dc7fc215303eb71f771796765661fc3
Thomas Santos <thomas@socialeffort.co>
2017-11-07 08:46:32 -0500

    add function queryDocument, commandLineOptions

--- WEEK_3/queryopsinnodejsdriver/app.js
+++ WEEK_3/queryopsinnodejsdriver/app.js
@@ -33,4 +33,41 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
   );
 });
 
+function queryDocument(options) {
+    console.log(options);
+    var query = {
+        "founded_year": {
+            "$gte": options.firstYear,
+            "$lte": options.lastYear
+        }
+    };
+
+    if ("employees" in options) {
+        query.number_of_employees = { "$gte": options.employees };
+    }
+
+    return query;
+}
+
+function commandLineOptions() {
+
+    var cli = commandLineArgs([
+        { name: "firstYear", alias: "f", type: Number },
+        { name: "lastYear", alias: "l", type: Number },
+        { name: "employees", alias: "e", type: Number }
+    ]);
+
+    var options = cli.parse()
+    if ( !(("firstYear" in options) && ("lastYear" in options))) {
+        console.log(cli.getUsage({
+            title: "Usage",
+            description: "The first two options below are required. The rest are optional."
+        }));
+        process.exit();
+    }
+
+    return options;
+
+}
+
 
