7fb11c1a786d6444925de9d8837cb42e59ae61c5
Thomas Santos <thomas@socialeffort.co>
2017-11-07 09:03:17 -0500

    refactor app for older cla pkg

--- WEEK_3/queryopsinnodejsdriver/app.js
+++ WEEK_3/queryopsinnodejsdriver/app.js
@@ -1,40 +1,41 @@
-// require mongodb, commanad-line-args, and assert packages
 var MongoClient = require('mongodb').MongoClient;
 var commandLineArgs = require('command-line-args');
 var assert = require('assert');
 
-// function commandLineOptions
 var options = commandLineOptions();
 
 MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
 
-  assert.equal(err, null);
-  console.log("Connected to MongoDB.");
+    assert.equal(err, null);
+    console.log("Successfully connected to MongoDB.");
 
-  var query = queryDocument(options);
-  // specific layout 'ask' for Mongo
-  var projection = {"_id": 1, "name": 1, "founded_year": 1, "number_of_employees": 1, "crunchbase_url": 1};
+    var query = queryDocument(options);
+    var projection = {"_id": 1, "name": 1, "founded_year": 1,
+                      "number_of_employees": 1, "crunchbase_url": 1};
 
+    var cursor = db.collection('companies').find(query, projection);
+    var numMatches = 0;
 
-  var cursor = db.collection('companies').find(query, projection);
-  var numMatches = 0;
+    cursor.forEach(
+        function(doc) {
+            numMatches = numMatches + 1;
+            console.log( doc );
+        },
+        function(err) {
+            assert.equal(err, null);
+            console.log("Our query was:" + JSON.stringify(query));
+            console.log("Matching documents: " + numMatches);
+            return db.close();
+        }
+    );
 
-  cursor.forEach(
-    function(doc) {
-      numMatches = numMatches + 1;
-      console.log(doc);
-    },
-    function(err) {
-      assert.equal(err, null);
-      console.log("Our query was:" + JSON.stringify(query));
-      console.log("Matching documents: " + numMatches);
-      return db.close();
-    }
-  );
 });
 
+
 function queryDocument(options) {
+
     console.log(options);
+
     var query = {
         "founded_year": {
             "$gte": options.firstYear,
@@ -47,7 +48,9 @@ function queryDocument(options) {
     }
 
     return query;
+
 }
+
 
 function commandLineOptions() {
 
@@ -57,7 +60,7 @@ function commandLineOptions() {
         { name: "employees", alias: "e", type: Number }
     ]);
 
-    var options = cli.parse()
+    var options = cli.parse();
     if ( !(("firstYear" in options) && ("lastYear" in options))) {
         console.log(cli.getUsage({
             title: "Usage",
