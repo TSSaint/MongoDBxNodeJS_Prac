1c0d4009e30ca97d35f1e006715a6547a85b77b1
Thomas Santos <thomas@socialeffort.co>
2017-11-07 00:46:16 -0500

    add prompt for hw3-1.js

---
+++ WEEK_3/00_Homework/hw3-1.js
@@ -0,0 +1,8 @@
+// For the space marked with a TODO comment below,
+// write ONE LINE OF CODE that will cause only the
+// name and number_of_employees fields to be returned
+// in query results. To simplify, please do not assign
+// your projection document to a variable as we
+// did in the lesson. Instead, just type the correct
+// projection document directly into the call to the
+// appropriate method.
